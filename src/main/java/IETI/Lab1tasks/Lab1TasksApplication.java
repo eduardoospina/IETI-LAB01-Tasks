edde0c8552eb6090bddf9d4cd197d75116929569
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-08-28 14:50:39 -0500

    ccreacion proyecto

---
+++ src/main/java/IETI/Lab1tasks/Lab1TasksApplication.java
@@ -0,0 +1,13 @@
+package IETI.Lab1tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1TasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1TasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IETI/Lab1tasks/Lab1TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package IETI.Lab1tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
